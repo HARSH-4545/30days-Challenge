7ff811ef261baee0635c08c0ff8d6b26380ed4d2
HARSH YADAV <149269219+HARSH-4545@users.noreply.github.com>
2024-05-23 23:22:38 +0530

    Create simpleObject.java

---
+++ simpleObject.java
@@ -0,0 +1,14 @@
+public class SimpleObject {
+
+    // Constructor of SimpleObject class
+    public SimpleObject() {
+        // Display a message
+        System.out.println("Hello! This is a message from the SimpleObject constructor.");
+    }
+
+    // Main method to create an instance of SimpleObject
+    public static void main(String[] args) {
+        // Creating an instance of SimpleObject
+        SimpleObject obj = new SimpleObject();
+    }
+}
